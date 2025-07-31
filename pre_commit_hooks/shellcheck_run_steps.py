bd7e7ae230a7c975388aa0358e420b2c06218192
dann frazier <dann.frazier@chainguard.dev>
2025-07-31 10:50:29 -0600

    shellcheck_run_steps: please grype upstream shellcheck images
    Signed-off-by: dann frazier <dann.frazier@chainguard.dev>

--- pre_commit_hooks/shellcheck_run_steps.py
+++ pre_commit_hooks/shellcheck_run_steps.py
@@ -13,7 +13,8 @@ import ruamel.yaml
 
 yaml = ruamel.yaml.YAML(typ="safe")
 
-# Reference by SHA for safety
+# Please provide the output of `grype koalaman/shellcheck@sha256:<newhash>`
+# in your PR when bumping. Referenced by SHA for safety.
 DefaultShellCheckImage = "koalaman/shellcheck@sha256:652a5a714dc2f5f97e36f565d4f7d2322fea376734f3ec1b04ed54ce2a0b124f"
 
 
